43e4aab59519144b81f9f097e2de2d7cfe1e5640
supissara-372 <supissaramukwin@gmail.com>
2025-09-27 22:36:49 +0700

    deploy5

--- quasar.config.js
+++ quasar.config.js
@@ -36,7 +36,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      publicPath: '/Versioncontrol2/',
+      publicPath: '/versioncontrol2/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
